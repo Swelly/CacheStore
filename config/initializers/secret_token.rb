08b1f6669a7edcb5d40b1254fe4d060fd0c338ef
Wesley Carr <surfswell17@gmail.com>
2013-08-23 14:30:39 -0400

    Models. Ran Migrations. Seeded db- User, product, review

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CacheStore::Application.config.secret_key_base = 'bede254d4dd89630717a7477678e2b4579118b4f06df7c9d2075388df1be7054ae21a5d45590494f88ea7e37abb229169b2c61ed4b7f81f9ed7edbfacd0a95cd'
