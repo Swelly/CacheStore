b0c28531ed4211f5b43042881024a65fd090aaad
Wesley Carr <surfswell17@gmail.com>
2013-08-23 14:58:27 -0400

    Enabling caching (in dev) for product show pages

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,7 +11,10 @@ CacheStore::Application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
-  config.action_controller.perform_caching = false
+
+  # Enable caching with dalli
+  config.action_controller.perform_caching = true
+  config.cache_store = :dalli_store
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
